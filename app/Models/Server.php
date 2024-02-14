c9d93aff8d951d6c5b03b5bc42a446b3ce9dc8b1
Adnan <dnanfab@gmail.com>
2024-02-14 21:19:11 +0700

    feat: servers model get usersMarzban

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -10,4 +10,9 @@ class Server extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function usersMarzban()
+    {
+        return $this->hasMany(UsersMarzban::class);
+    }
 }
